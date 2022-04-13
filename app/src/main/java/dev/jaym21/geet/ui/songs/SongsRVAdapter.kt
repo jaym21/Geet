4e7d985c8c29b868deea98bfd69cda7152283cb0
jaym21 <jaynmistry21@gmail.com>
2022-04-13 12:04:46 +0530

    added album artwork

--- app/src/main/java/dev/jaym21/geet/ui/songs/SongsRVAdapter.kt
+++ app/src/main/java/dev/jaym21/geet/ui/songs/SongsRVAdapter.kt
@@ -1,13 +1,17 @@
 package dev.jaym21.geet.ui.songs
 
+import android.content.ContentUris
+import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import dev.jaym21.geet.R
 import dev.jaym21.geet.databinding.ItemSongLayoutBinding
 import dev.jaym21.geet.models.Song
@@ -27,6 +31,7 @@ class SongsRVAdapter: ListAdapter<Song, SongsRVAdapter.SongsViewHolder>(SongsDif
     inner class SongsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val title: TextView = itemView.findViewById(R.id.tvSongTitle)
         val artist: TextView = itemView.findViewById(R.id.tvArtistName)
+        val artwork: ImageView = itemView.findViewById(R.id.ivSongArtwork)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongsViewHolder {
@@ -37,5 +42,10 @@ class SongsRVAdapter: ListAdapter<Song, SongsRVAdapter.SongsViewHolder>(SongsDif
         val currentItem = getItem(position)
         holder.title.text = currentItem.title
         holder.artist.text = currentItem.artist
+
+        val artworkUri = Uri.parse("content://media/external/audio/albumart")
+        val albumArtUri = ContentUris.withAppendedId(artworkUri,currentItem.albumId)
+
+        Glide.with(holder.itemView.context).load(albumArtUri).into(holder.artwork)
     }
 }
---
+++ app/src/main/java/dev/jaym21/geet/utils/PreferencesHelper.kt
@@ -0,0 +1,19 @@
+package dev.jaym21.geet.utils
+
+import android.app.Activity
+import android.content.Context
+
+object PreferencesHelper {
+
+    fun setSongSortOrder(context: Context, sortOrder: String) {
+        val sharedPreferences = context.getSharedPreferences(Constants.PREFERENCES_HELPER, Activity.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(Constants.SONG_SORT_ORDER, sortOrder)
+        editor.apply()
+    }
+
+    fun getSongSortOrder(context: Context): String? {
+        val sharedPreferences = context.getSharedPreferences(Constants.PREFERENCES_HELPER, Activity.MODE_PRIVATE)
+        return sharedPreferences.getString(Constants.SONG_SORT_ORDER, "")
+    }
+}
