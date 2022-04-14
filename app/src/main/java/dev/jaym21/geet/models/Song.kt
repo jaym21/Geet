3a9b64115961077efdf5674751a14883cbd2a318
jaym21 <jaynmistry21@gmail.com>
2022-04-14 12:59:19 +0530

    updated song data class to parcelable

--- app/src/main/java/dev/jaym21/geet/models/Song.kt
+++ app/src/main/java/dev/jaym21/geet/models/Song.kt
@@ -1,5 +1,11 @@
 package dev.jaym21.geet.models
 
+import android.os.Parcelable
+import androidx.annotation.Keep
+import kotlinx.parcelize.Parcelize
+
+@Keep
+@Parcelize
 data class Song(
     var id: Long = 0,
     var title: String = "",
@@ -9,4 +15,4 @@ data class Song(
     var albumId: Long = 0,
     var artistId: Long = 0,
     var trackNumber: Int = 0
-)
+): Parcelable
