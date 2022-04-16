17dcd5792ae1644bcf27b0f87f28d10ab640a47d
jaym21 <jaynmistry21@gmail.com>
2022-04-16 14:40:49 +0530

    added gson dependency, preferences and constants

--- app/src/main/java/dev/jaym21/geet/utils/Constants.kt
+++ app/src/main/java/dev/jaym21/geet/utils/Constants.kt
@@ -2,8 +2,19 @@ package dev.jaym21.geet.utils
 
 object Constants {
 
+    //shared preferences
     const val PREFERENCES_HELPER = "preferences_helper"
     const val SONG_SORT_ORDER = "song_sort_order"
+    const val QUEUE_IDS = "queue_ids"
+
+    //permissions
     const val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 100
     const val READ_EXTERNAL_STORAGE_REQUEST_CODE = 101
+
+    //services
+    const val SONG_LOADED = "song_loaded"
+    const val SONG_STARTED = "song_started"
+    const val SONG_PLAYED = "song_played"
+    const val SONG_PAUSED = "song_paused"
+    const val SONG_ENDED = "song_ended"
 }
--- app/src/main/java/dev/jaym21/geet/utils/PreferencesHelper.kt
+++ app/src/main/java/dev/jaym21/geet/utils/PreferencesHelper.kt
@@ -2,6 +2,7 @@ package dev.jaym21.geet.utils
 
 import android.app.Activity
 import android.content.Context
+import com.google.gson.Gson
 
 object PreferencesHelper {
 
@@ -14,6 +15,31 @@ object PreferencesHelper {
 
     fun getSongSortOrder(context: Context): String? {
         val sharedPreferences = context.getSharedPreferences(Constants.PREFERENCES_HELPER, Activity.MODE_PRIVATE)
-        return sharedPreferences.getString(Constants.SONG_SORT_ORDER, "")
+        return sharedPreferences.getString(Constants.SONG_SORT_ORDER, null)
+    }
+
+    fun setQueueIds(context: Context, ids: LongArray) {
+        val sharedPreferences = context.getSharedPreferences(Constants.PREFERENCES_HELPER, Activity.MODE_PRIVATE)
+        val gson = Gson()
+        val idsArrayString = gson.toJson(ids)
+        val editor = sharedPreferences.edit()
+        editor.putString(Constants.QUEUE_IDS, idsArrayString)
+        editor.apply()
+    }
+
+    fun getQueueIds(context: Context): LongArray {
+        val sharedPreferences = context.getSharedPreferences(Constants.PREFERENCES_HELPER, Activity.MODE_PRIVATE)
+        val idsArrayStringJson = sharedPreferences.getString(Constants.QUEUE_IDS, null)
+        val gson = Gson()
+        val list = gson.fromJson(idsArrayStringJson, ArrayList::class.java)
+        val longList = mutableListOf<Long>()
+        if (list != null) {
+            for (i in list) {
+                val d = i as Double
+                val l = d.toLong()
+                longList.add(l)
+            }
+        }
+        return longList.toLongArray()
     }
 }
