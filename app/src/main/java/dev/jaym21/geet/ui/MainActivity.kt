28c80f005edccee952d32f203c55866475f01076
jaym21 <jaynmistry21@gmail.com>
2022-04-20 14:42:48 +0530

    initialized view pager adapter and tab layout in main activity

--- app/src/main/java/dev/jaym21/geet/ui/MainActivity.kt
+++ app/src/main/java/dev/jaym21/geet/ui/MainActivity.kt
@@ -2,7 +2,9 @@ package dev.jaym21.geet.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
 import dev.jaym21.geet.R
+import dev.jaym21.geet.adapters.MainViewPagerAdapter
 import dev.jaym21.geet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding: ActivityMainBinding
         get() = _binding!!
+    private lateinit var mainViewPagerAdapter: MainViewPagerAdapter
     var tabs = arrayOf("Songs", "Albums", "Artists", "Genres", "Playlists")
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +20,14 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mainViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager, lifecycle)
 
+        binding.vpMain.adapter = mainViewPagerAdapter
+
+        //integrating tabLayout
+        TabLayoutMediator(binding.tlMain, binding.vpMain) { tab, position ->
+            tab.text = tabs[position]
+        }.attach()
     }
 
     override fun onDestroy() {
