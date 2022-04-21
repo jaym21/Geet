19e1f256b20b0dc94fa95ae6914520053c314155
jaym21 <jaynmistry21@gmail.com>
2022-04-21 22:49:07 +0530

    added dao functions for queue and queued songs

--- app/src/main/java/dev/jaym21/geet/db/QueueDAO.kt
+++ app/src/main/java/dev/jaym21/geet/db/QueueDAO.kt
@@ -1,12 +1,29 @@
 package dev.jaym21.geet.db
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
+import androidx.room.*
 import dev.jaym21.geet.models.QueueEntity
+import dev.jaym21.geet.models.QueuedSongsEntity
 
 @Dao
 interface QueueDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertQueue(queue: QueueEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllSongs(songs: List<QueuedSongsEntity>)
+
+    @Query("DELETE from queued_songs")
+    fun clearQueueSongs()
+
+    @Delete
+    fun deleteSongFromQueue(song: QueuedSongsEntity)
+
+    @Query("SELECT * FROM queue_table where id = 0")
+    fun getQueue(): QueueEntity?
+
+    @Query("SELECT * FROM queue_songs")
+    fun getQueuedSongs(): List<QueuedSongsEntity>
+
+    @Query("UPDATE queue_table SET currentId  = :currentId where id = 0")
+    fun setCurrentId(currentId: Long)
 }
