2cb68d6cdd61d86af210d3897ea379b5753e6239
jaym21 <jaynmistry21@gmail.com>
2022-04-22 11:34:32 +0530

    added extension functions for song, data type and collections

--- app/src/main/java/dev/jaym21/geet/extensions/CollectionsExtensions.kt
+++ app/src/main/java/dev/jaym21/geet/extensions/CollectionsExtensions.kt
@@ -1,5 +1,9 @@
 package dev.jaym21.geet.extensions
 
+import android.support.v4.media.session.MediaSessionCompat
+import dev.jaym21.geet.models.Song
+import dev.jaym21.geet.repository.SongsRepository
+
 fun <T> List<T>?.moveElement(fromIndex: Int, toIndex: Int): List<T> {
     if (this == null) {
         return emptyList()
@@ -7,4 +11,25 @@ fun <T> List<T>?.moveElement(fromIndex: Int, toIndex: Int): List<T> {
     return toMutableList().apply {
         add(toIndex, removeAt(fromIndex))
     }
+}
+
+fun LongArray.toQueue(songsRepository: SongsRepository): List<MediaSessionCompat.QueueItem> {
+    val songList = songsRepository.getSongsForIds(this)
+    //keeping the original order of songs as the ids array
+    songList.keepInOrder(this)?.let {
+        return it.toQueue()
+    }?: return songList.toQueue()
+}
+
+fun List<Song>.keepInOrder(queue: LongArray): List<Song>? {
+    //this may happen if user deletes some item from his library and then comes back to app after we stored the current queue ids
+    //if the two arrays are different return the array as is
+    if (size != queue.size) return this
+    return if (isNotEmpty() && queue.isNotEmpty()) {
+        val keepOrderList = Array(size, init = { Song() })
+        forEach {
+            keepOrderList[queue.indexOf(it.id)] = it
+        }
+        keepOrderList.asList()
+    } else null
 }
