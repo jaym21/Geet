3ca512983c3898d3ea49bc3a6e5ef402038925ae
jaym21 <jaynmistry21@gmail.com>
2022-04-23 22:41:47 +0530

    added album data class

---
+++ app/src/main/java/dev/jaym21/geet/models/Album.kt
@@ -0,0 +1,24 @@
+package dev.jaym21.geet.models
+
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaDescriptionCompat
+import dev.jaym21.geet.utils.Constants
+import dev.jaym21.geet.utils.SongUtils
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Album(
+    var id: Long = 0,
+    var title: String = "",
+    var artist: String = "",
+    var artistId: Long = 0,
+    var noOfSongs: Int = 0,
+    var year: Int = 0
+) : MediaBrowserCompat.MediaItem(
+    MediaDescriptionCompat.Builder()
+        .setMediaId(MediaID(Constants.ALBUM_MODE.toString(), id.toString()).asString())
+        .setTitle(title)
+        .setIconUri(SongUtils.getAlbumArtUri(id))
+        .setSubtitle(artist)
+        .build(), FLAG_BROWSABLE
+)
