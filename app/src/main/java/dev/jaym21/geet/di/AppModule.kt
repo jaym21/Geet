3375495a11fdd740c6f7cf1331cdf6a2862e5bf2
jaym21 <jaynmistry21@gmail.com>
2022-04-23 12:59:07 +0530

    added app module with database and song repository injection functions

---
+++ app/src/main/java/dev/jaym21/geet/di/AppModule.kt
@@ -0,0 +1,26 @@
+package dev.jaym21.geet.di
+
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.jaym21.geet.db.GeetDatabase
+import dev.jaym21.geet.repository.SongsRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(application: Application): GeetDatabase =
+        Room.databaseBuilder(application, GeetDatabase::class.java, "geet_database").build()
+
+    @Provides
+    @Singleton
+    fun provideSongRepository(application: Application): SongsRepository =
+        SongsRepository(application.applicationContext)
+}
