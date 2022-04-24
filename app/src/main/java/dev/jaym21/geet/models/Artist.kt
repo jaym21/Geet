e0a08abd1319e516844aa69fdef9cc8aa628162f
jaym21 <jaynmistry21@gmail.com>
2022-04-24 11:07:55 +0530

    added artist data class

---
+++ app/src/main/java/dev/jaym21/geet/models/Artist.kt
@@ -0,0 +1,19 @@
+package dev.jaym21.geet.models
+
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaDescriptionCompat
+import dev.jaym21.geet.utils.Constants
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Artist (
+    var id: Long = 0,
+    var name: String = "",
+    var noOfSongs: Int = 0,
+    var noOfAlbums: Int = 0
+) : MediaBrowserCompat.MediaItem(
+        MediaDescriptionCompat.Builder()
+            .setMediaId(MediaID(Constants.ARTIST_MODE.toString(), id.toString()).asString())
+            .setTitle(name)
+            .setSubtitle("$noOfAlbums albums")
+            .build(), FLAG_BROWSABLE)
