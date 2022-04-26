757645dc659a50debe57e7c480d3a15726ec52de
jaym21 <jaynmistry21@gmail.com>
2022-04-26 13:00:24 +0530

    adding case to show and hide bottom sheet using current data from now playing viewModel

--- app/src/main/java/dev/jaym21/geet/extensions/DataTypeExtensions.kt
+++ app/src/main/java/dev/jaym21/geet/extensions/DataTypeExtensions.kt
@@ -1,5 +1,8 @@
 package dev.jaym21.geet.extensions
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
 fun Int.normalizeTrackNumber(): Int {
     var returnValue = this
     while (returnValue >= 1000) {
@@ -7,3 +10,6 @@ fun Int.normalizeTrackNumber(): Int {
     }
     return returnValue
 }
+
+inline val Fragment.safeActivity: FragmentActivity
+    get() = activity ?: throw IllegalStateException("Fragment not attached")
--- app/src/main/java/dev/jaym21/geet/ui/BaseFragment.kt
+++ app/src/main/java/dev/jaym21/geet/ui/BaseFragment.kt
@@ -1,6 +1,40 @@
 package dev.jaym21.geet.ui
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import dev.jaym21.geet.R
+import dev.jaym21.geet.extensions.safeActivity
 
-class BaseFragment: Fragment() {
+open class BaseFragment: Fragment() {
+
+    protected val mainViewModel by activityViewModels<MainViewModel>()
+    protected val nowPlayingViewModel by activityViewModels<NowPlayingViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        nowPlayingViewModel.currentData.observe(this) {
+          changeBottomSheetState()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    private fun changeBottomSheetState() {
+        nowPlayingViewModel.currentData.value?.let {
+            val activity = safeActivity as MainActivity
+            if (!it.title.isNullOrEmpty()) {
+                if (activity.supportFragmentManager.findFragmentById(R.id.mainContainer) is NowPlayingFragment) {
+                    activity.hideBottomSheet()
+                } else {
+                    activity.showBottomSheet()
+                }
+            } else {
+                activity.hideBottomSheet()
+            }
+        }
+    }
 }
