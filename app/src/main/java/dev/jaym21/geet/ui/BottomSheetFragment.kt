d5c558560f319645e03f29a291ec67133d268f5e
jaym21 <jaynmistry21@gmail.com>
2022-04-26 11:13:04 +0530

    added layout for bottom sheet

---
+++ app/src/main/java/dev/jaym21/geet/ui/BottomSheetFragment.kt
@@ -0,0 +1,23 @@
+package dev.jaym21.geet.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.jaym21.geet.R
+
+class BottomSheetFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
+    }
+
+    companion object {
+
+    }
+}
