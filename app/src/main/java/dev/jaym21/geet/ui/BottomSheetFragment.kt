70520cb533dad0cb3f6755311d2d32d996686898
jaym21 <jaynmistry21@gmail.com>
2022-04-26 11:28:49 +0530

    updated layout of bottom sheet

--- app/src/main/java/dev/jaym21/geet/ui/BottomSheetFragment.kt
+++ app/src/main/java/dev/jaym21/geet/ui/BottomSheetFragment.kt
@@ -6,18 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dev.jaym21.geet.R
+import dev.jaym21.geet.databinding.FragmentBottomSheetBinding
 
 class BottomSheetFragment : Fragment() {
+
+    private var _binding: FragmentBottomSheetBinding? = null
+    private val binding: FragmentBottomSheetBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
+        _binding = FragmentBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
