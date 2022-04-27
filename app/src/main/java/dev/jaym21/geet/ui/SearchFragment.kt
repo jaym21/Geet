6edca059dc166a08e4d119b139b1ebfa3a7ea7d6
jaym21 <jaynmistry21@gmail.com>
2022-04-27 10:30:27 +0530

    added search fragment

---
+++ app/src/main/java/dev/jaym21/geet/ui/SearchFragment.kt
@@ -0,0 +1,25 @@
+package dev.jaym21.geet.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.jaym21.geet.R
+import dev.jaym21.geet.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding: FragmentSearchBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
