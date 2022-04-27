507c8c6a2bc7e6deffb6dae27155d2f2708e4f6f
jaym21 <jaynmistry21@gmail.com>
2022-04-27 10:25:13 +0530

    added home fragment

---
+++ app/src/main/java/dev/jaym21/geet/ui/HomeFragment.kt
@@ -0,0 +1,29 @@
+package dev.jaym21.geet.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.jaym21.geet.R
+import dev.jaym21.geet.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
