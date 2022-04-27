5551df1b04fff7a813cd02edadd4d1e159b873ce
jaym21 <jaynmistry21@gmail.com>
2022-04-27 18:50:03 +0530

    updated base fragment

--- app/src/main/java/dev/jaym21/geet/ui/BaseFragment.kt
+++ app/src/main/java/dev/jaym21/geet/ui/BaseFragment.kt
@@ -6,35 +6,17 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import dev.jaym21.geet.R
 import dev.jaym21.geet.extensions.safeActivity
+import dev.jaym21.geet.viewmodels.MainViewModel
+import dev.jaym21.geet.viewmodels.NavigationViewModel
+import dev.jaym21.geet.viewmodels.NowPlayingViewModel
 
 open class BaseFragment: Fragment() {
 
     protected val mainViewModel by activityViewModels<MainViewModel>()
+    protected val navigationViewModel by activityViewModels<NavigationViewModel>()
     protected val nowPlayingViewModel by activityViewModels<NowPlayingViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        nowPlayingViewModel.currentData.observe(this) {
-          changeBottomSheetState()
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    private fun changeBottomSheetState() {
-        nowPlayingViewModel.currentData.value?.let {
-            val activity = safeActivity as MainActivity
-            if (!it.title.isNullOrEmpty()) {
-                if (activity.supportFragmentManager.findFragmentById(R.id.mainContainer) is NowPlayingFragment) {
-                    activity.hideBottomSheet()
-                } else {
-                    activity.showBottomSheet()
-                }
-            } else {
-                activity.hideBottomSheet()
-            }
-        }
     }
 }
