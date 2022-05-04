358ac76c056c198d9fc788380ca61010245b0cb6
jaym21 <jaynmistry21@gmail.com>
2022-05-04 18:17:37 +0530

    added implementation for artist details fragment

--- app/src/main/java/dev/jaym21/geet/ui/details/ArtistDetailsFragment.kt
+++ app/src/main/java/dev/jaym21/geet/ui/details/ArtistDetailsFragment.kt
@@ -1,21 +1,178 @@
 package dev.jaym21.geet.ui.details
 
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import dev.jaym21.geet.R
+import dev.jaym21.geet.adapters.ArtistAlbumsRVAdapter
+import dev.jaym21.geet.adapters.IArtistAlbumsRVAdapter
+import dev.jaym21.geet.adapters.ISongsRVAdapter
+import dev.jaym21.geet.adapters.SongsRVAdapter
+import dev.jaym21.geet.databinding.FragmentArtistDetailsBinding
+import dev.jaym21.geet.extensions.filter
+import dev.jaym21.geet.extensions.getExtraBundle
+import dev.jaym21.geet.extensions.toSongIds
+import dev.jaym21.geet.models.Album
+import dev.jaym21.geet.models.Artist
+import dev.jaym21.geet.models.Song
+import dev.jaym21.geet.ui.BaseFragment
+import dev.jaym21.geet.utils.Constants
+import dev.jaym21.geet.utils.SongUtils
 
 
-class ArtistDetailsFragment : Fragment() {
+class ArtistDetailsFragment : BaseFragment(), IArtistAlbumsRVAdapter, ISongsRVAdapter {
+
+    private var _binding: FragmentArtistDetailsBinding? = null
+    private val binding: FragmentArtistDetailsBinding
+        get() = _binding!!
+    private var artist: Artist? = null
+    private var caller: String? = null
+    private var songs = listOf<Song>()
+    private var songsAdapter: SongsRVAdapter? = null
+    private var artistAlbumAdapter: ArtistAlbumsRVAdapter = ArtistAlbumsRVAdapter(this)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_artist_details, container, false)
+        _binding = FragmentArtistDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        artist = arguments?.getParcelable(Constants.ARTIST)
+        caller = arguments?.getString(Constants.MEDIA_CALLER)
+
+        if (artist != null && caller != null) {
+            binding.tvArtistName.text = artist?.name
+
+            var albumText = "album"
+            var songText = "song"
+            if (artist?.noOfAlbums!! > 1) {
+                albumText = "albums"
+            }
+            if (artist?.noOfSongs!! > 1) {
+                songText = "songs"
+            }
+            binding.tvArtistInfo.text = context?.getString(
+                R.string.format_two,
+                "${artist?.noOfAlbums} $albumText",
+                "${artist?.noOfSongs} $songText"
+            )
+
+            artist?.albumIds?.let { addArtwork(it) }
+
+            songsAdapter = SongsRVAdapter(this, this, nowPlayingViewModel)
+
+            setUpRecyclerView()
+
+            //getting songs for artist
+            mainViewModel.getArtistSongs(caller!!, artist?.id!!)
+
+            mainViewModel.artistSongs.observe(viewLifecycleOwner) {
+                songs = it
+                songsAdapter?.submitList(it)
+            }
+
+            //getting albums for artist
+            mainViewModel.getArtistAlbums(artist?.id!!)
+
+            mainViewModel.artistAlbums.observe(viewLifecycleOwner) {
+                artistAlbumAdapter.submitList(it)
+            }
+
+            playbackSessionViewModel?.mediaItems
+                ?.filter { it.isNotEmpty() }
+                ?.observe(this) {
+                    songsAdapter?.submitList(it as List<Song>)
+                }
+
+            binding.ivBackButton.setOnClickListener {
+                findNavController().popBackStack()
+            }
+
+        } else {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun addArtwork(albumIds: List<Long>) {
+        val images = mutableListOf<Bitmap>()
+        for (i in albumIds) {
+            val bitmap = SongUtils.getAlbumArtBitmap(requireContext(), i)
+            if (bitmap != null) {
+                images.add(bitmap)
+            }
+        }
+
+        when (images.size) {
+            0 -> {
+                binding.llArtworkBottomLayer.visibility = View.GONE
+                binding.ivAlbumsArtwork2.visibility = View.GONE
+                Glide.with(requireContext()).load(R.drawable.ic_album_disk).into(binding.ivAlbumsArtwork1)
+                binding.ivAlbumsArtwork1.setColorFilter(ContextCompat.getColor(requireContext(), R.color.colorAccent))
+            }
+            1 -> {
+                binding.llArtworkBottomLayer.visibility = View.GONE
+                binding.ivAlbumsArtwork2.visibility = View.GONE
+                Glide.with(requireContext()).load(images[0]).into(binding.ivAlbumsArtwork1)
+            }
+            2 -> {
+                binding.llArtworkBottomLayer.visibility = View.GONE
+                Glide.with(requireContext()).load(images[0]).into(binding.ivAlbumsArtwork1)
+                Glide.with(requireContext()).load(images[1]).into(binding.ivAlbumsArtwork2)
+            }
+            3 -> {
+                binding.ivAlbumsArtwork4.visibility = View.GONE
+                Glide.with(requireContext()).load(images[0]).into(binding.ivAlbumsArtwork1)
+                Glide.with(requireContext()).load(images[1]).into(binding.ivAlbumsArtwork2)
+                Glide.with(requireContext()).load(images[2]).into(binding.ivAlbumsArtwork3)
+            }
+            4 -> {
+                Glide.with(requireContext()).load(images[0]).into(binding.ivAlbumsArtwork1)
+                Glide.with(requireContext()).load(images[1]).into(binding.ivAlbumsArtwork2)
+                Glide.with(requireContext()).load(images[2]).into(binding.ivAlbumsArtwork3)
+                Glide.with(requireContext()).load(images[3]).into(binding.ivAlbumsArtwork4)
+            }
+            else -> {
+                Glide.with(requireContext()).load(images[0]).into(binding.ivAlbumsArtwork1)
+                Glide.with(requireContext()).load(images[1]).into(binding.ivAlbumsArtwork2)
+                Glide.with(requireContext()).load(images[2]).into(binding.ivAlbumsArtwork3)
+                Glide.with(requireContext()).load(images[3]).into(binding.ivAlbumsArtwork4)
+            }
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        binding.apply {
+            rvArtistSongs.adapter = songsAdapter
+            rvArtistSongs.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            rvArtistAlbums.adapter = artistAlbumAdapter
+            rvArtistAlbums.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
+    }
+
+    override fun onArtistAlbumClick(album: Album) {
+        mainViewModel.mediaItemClicked(album, null)
+    }
+
+    override fun onSongClicked(song: Song) {
+        val extras = getExtraBundle(songs.toSongIds(), artist?.name!!)
+        mainViewModel.mediaItemClicked(song, extras)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
