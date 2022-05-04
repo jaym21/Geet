8cc15eb784679cbca19d306f30b78384aefa6436
jaym21 <jaynmistry21@gmail.com>
2022-05-04 09:59:10 +0530

    added layout for album details fragment

--- app/src/main/java/dev/jaym21/geet/ui/details/AlbumDetailsFragment.kt
+++ app/src/main/java/dev/jaym21/geet/ui/details/AlbumDetailsFragment.kt
@@ -6,15 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dev.jaym21.geet.R
+import dev.jaym21.geet.databinding.FragmentAlbumDetailsBinding
 
 
 class AlbumDetailsFragment : Fragment() {
 
+    private var _binding: FragmentAlbumDetailsBinding? = null
+    private val binding: FragmentAlbumDetailsBinding
+        get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_album_details, container, false)
+        _binding = FragmentAlbumDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
