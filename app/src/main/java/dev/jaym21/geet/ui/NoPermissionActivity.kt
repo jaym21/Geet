e2b3ad6c4c7fc980be9476304025117f21632276
jaym21 <jaynmistry21@gmail.com>
2022-05-04 19:30:40 +0530

    added no permission activity

---
+++ app/src/main/java/dev/jaym21/geet/ui/NoPermissionActivity.kt
@@ -0,0 +1,12 @@
+package dev.jaym21.geet.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.jaym21.geet.R
+
+class NoPermissionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_no_permission)
+    }
+}
