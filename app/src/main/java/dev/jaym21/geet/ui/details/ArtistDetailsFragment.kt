1a289d1f9fd7152630e4b79a6363a35c4f74dfc7
jaym21 <jaynmistry21@gmail.com>
2022-05-04 09:21:18 +0530

    added artist details fragment

---
+++ app/src/main/java/dev/jaym21/geet/ui/details/ArtistDetailsFragment.kt
@@ -0,0 +1,21 @@
+package dev.jaym21.geet.ui.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.jaym21.geet.R
+
+
+class ArtistDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_artist_details, container, false)
+    }
+
+}
