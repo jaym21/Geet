4d6d4e3e6a5f9979b7ad9a66275e3bdb6631d419
jaym21 <jaynmistry21@gmail.com>
2022-05-04 09:09:54 +0530

    added album details fragment

---
+++ app/src/main/java/dev/jaym21/geet/ui/details/AlbumDetailsFragment.kt
@@ -0,0 +1,20 @@
+package dev.jaym21.geet.ui.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.jaym21.geet.R
+
+
+class AlbumDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_album_details, container, false)
+    }
+}
