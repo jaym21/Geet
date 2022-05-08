e89b30f055548f4e73bd3a81d5cd7f18504e5e4e
jaym21 <jaynmistry21@gmail.com>
2022-05-08 10:45:57 +0530

    implemented showing all playlists and playlist click

--- app/src/main/java/dev/jaym21/geet/ui/home/PlaylistsFragment.kt
+++ app/src/main/java/dev/jaym21/geet/ui/home/PlaylistsFragment.kt
@@ -5,16 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import dev.jaym21.geet.R
+import dev.jaym21.geet.adapters.IPlaylistsRVAdapter
+import dev.jaym21.geet.adapters.PlaylistsRVAdapter
+import dev.jaym21.geet.databinding.FragmentPlaylistsBinding
+import dev.jaym21.geet.extensions.filter
+import dev.jaym21.geet.models.Playlist
+import dev.jaym21.geet.ui.BaseFragment
 
 
-class PlaylistsFragment : Fragment() {
+class PlaylistsFragment : BaseFragment(), IPlaylistsRVAdapter {
+
+    private var _binding: FragmentPlaylistsBinding? = null
+    private val binding: FragmentPlaylistsBinding
+        get() = _binding!!
+    private val playlistAdapter = PlaylistsRVAdapter(this)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_playlists, container, false)
+        _binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpRecyclerView()
+
+        mainViewModel.loadPlaylists()
+
+        mainViewModel.playlists.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty()) {
+                binding.tvNoPlaylistAdded.visibility = View.GONE
+            }
+            playlistAdapter.submitList(it)
+        }
+
+        playbackSessionViewModel?.mediaItems
+            ?.filter { it.isNotEmpty() }
+            ?.observe(this) { list ->
+                @Suppress("UNCHECKED_CAST")
+                playlistAdapter.submitList(list as List<Playlist>)
+            }
+    }
+
+    private fun setUpRecyclerView() {
+        binding.apply {
+            rvAllPlaylists.adapter = playlistAdapter
+            rvAllPlaylists.layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+        }
+    }
+
+    override fun onPlaylistClicked(playlist: Playlist) {
+        mainViewModel.mediaItemClicked(playlist, null)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
